f7152672eeb5df8477a39df89c446d6281448ce5
Wenxin <24028453@myrp.edu.sg>
2026-05-29 23:51:33 +0800

    Add My Closet page

--- app.js
+++ app.js
@@ -7,8 +7,35 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+let clothes = [
+{
+id:1,
+imageUrl:"https://images.unsplash.com/photo-1521572267360-ee0c2909d518",
+name:"White T-Shirt",
+category:"Top",
+color:"White",
+style:"Casual"
+},
+{
+id:2,
+imageUrl:"https://images.unsplash.com/photo-1541099649105-f69ad21f3246",
+name:"Blue Jeans",
+category:"Bottom",
+color:"Blue",
+style:"Casual"
+}
+];
+
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/closet',(req,res)=>{
+
+res.render('closet',{
+clothes
+});
+
 });
 
 app.listen(port, () => {
